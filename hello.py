28be4251c8ca35c8a2e5124c077e30d969e4136f
hbys <email@example.com>
2019-03-23 17:31:26 +0800

    Flak chapter 4

---
+++ hello.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template,session,redirect,url_for,flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from datetime import datetime
+
+app = Flask(__name__)
+bootstrap=Bootstrap(app)
+moment=Moment(app)
+app.config['SECRET_KEY']='hard to guess string'
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?',validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like your changer your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+        #form.name.data=''
+    return render_template("index.html",current_time=datetime.utcnow(),form=form,name=session.get('name'))
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html",name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"),500
+
+
+if __name__ == '__main__':
+    app.run(Debug  =  True)
